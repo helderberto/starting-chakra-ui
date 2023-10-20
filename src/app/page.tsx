f97869826da97490a2e8e7451921fba54b1b5b08
Helder Burato Berto <helder.burato@gmail.com>
2023-10-20 14:49:04 +0100

    use sections components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
-import { Container, Flex, VStack } from "@chakra-ui/react";
+import { Cart, Details } from "@/components";
+import { Container, Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Container maxW="container.xl" p={0}>
       <Flex h="100vh" py={20}>
-        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
-          firt child
-        </VStack>
-        <VStack
-          w="full"
-          h="full"
-          p={10}
-          spacing={10}
-          alignItems="flex-start"
-          bg="gray.50"
-        >
-          second child
-        </VStack>
+        <Cart />
+        <Details />
       </Flex>
     </Container>
   );
