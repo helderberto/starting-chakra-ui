222f9ead3d22340c5019c8ab469f259e5cd10e6f
Helder Burato Berto <helder.burato@gmail.com>
2023-10-20 14:39:54 +0100

    add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Flex, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Container maxW="container.xl" p={0}>
-      dasdas
+      <Flex h="100vh" py={20}>
+        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+          firt child
+        </VStack>
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        >
+          second child
+        </VStack>
+      </Flex>
     </Container>
   );
 }
