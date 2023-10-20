fbeb89d443869f9d7fa906bc8b43312cc068680b
Helder Burato Berto <helder.burato@gmail.com>
2023-10-20 14:51:43 +0100

    invert the order of details & cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <Container maxW="container.xl" p={0}>
       <Flex h="100vh" py={20}>
-        <Cart />
         <Details />
+        <Cart />
       </Flex>
     </Container>
   );
