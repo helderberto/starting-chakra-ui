bbad058c4a7a41c6234c1da0d45ae356649d7f59
Helder Burato Berto <helder.burato@gmail.com>
2023-10-20 14:36:15 +0100

    add container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Container } from "@chakra-ui/react";
+
 export default function Home() {
-  return <main>Hello Next.js</main>;
+  return (
+    <Container maxW="container.xl" p={0}>
+      dasdas
+    </Container>
+  );
 }
